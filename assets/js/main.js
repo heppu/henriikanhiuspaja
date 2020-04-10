41477d99fc79b50a253ce03503349b51f0b95585
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-10 14:30:04 +0000

    Deploy henriikanhiuspaja.fi to github.com/heppu/henriikanhiuspaja.git:gh-pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,33 +5,6 @@
 */
 (function($) {
 
-    function initMap() {
-
-        var location = new google.maps.LatLng(64.25839, 23.94756);
-
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-            center: location,
-            zoom: 16,
-            panControl: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            zoomControl: false,
-			mapTypeControl: false,
-			scaleControl: false,
-			streetViewControl: false,
-			rotateControl: false,
-			fullscreenControl: false
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions);
-          var marker = new google.maps.Marker({
-		    position: location,
-		    map: map,
-		  });
-
-    }
-
-    google.maps.event.addDomListener(window, 'load', initMap);
-
 	skel.breakpoints({
 		wide: '(max-width: 1680px)',
 		normal: '(max-width: 1280px)',
